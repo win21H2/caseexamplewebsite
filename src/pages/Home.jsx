aa467ef321be13101be300730228d1ad39190b35
324Hz <1024Bitcoins@gmail.com>
2023-05-15 14:52:38 -0500

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,10 @@
 import { Link } from "react-router-dom"
 
 const Home = () => {
+
     return (
         <>
-            <div className="pageTitle">Hello World!</div>
-            <div className="infoPara">This is the style of website you will be able to make at the end of this CASE project!</div>
-            <Link to="/about" className="pageLink">About this CASE activity</Link>
+
         </>
     )
 }
